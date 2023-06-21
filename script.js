bf0d180f9d96e6f9b25c1e761f6cd4be7e86cb45
CrumpetCoding <seannrowe@gmail.com>
2023-06-21 13:26:54 +0100

    Added playerSelection and computerSelection functions

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 function getComputerChoice(){
     var word = ['Rock', 'Paper', 'Scissors'];
-    var output = word [Math.floor(Math.random() * word.length)];
-    return output;
+    return word [Math.floor(Math.random() * word.length)];
 }
 
-function getPlayerChoice(input){
-    let player = input.toLowerCase();
-    return player.charAt(0).toUpperCase() + player.slice(1);
+function playGame(playerSelection, computerSelection){
+
+    computerSelection = getComputerChoice().toLowerCase();
+
+    playerSelection = playerSelection.toLowerCase();
+
+    if (computerSelection == playerSelection)
+    {
+        console.log("TIE!");
+    }
+    else if (
+        (computerSelection == "rock" && playerSelection == "paper")
+        (computerSelection == "scissors" && playerSelection == "rock")
+        (computerSelection == "paper" && playerSelection == "scissors")
+    ){
+        console.log("You win! " + playerSelection + " beats " + computerSelection);
+    }
+    else if (
+        (computerSelection == "paper" && playerSelection == "rock")
+        (computerSelection == "rock" && playerSelection == "scissors")
+        (computerSelection == "scissors" && playerSelection == "paper")
+    ){
+        console.log("You lose... " + computerSelection +  " beats " + playerSelection);
+    }
 }   
